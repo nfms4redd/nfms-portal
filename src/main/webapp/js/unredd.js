bb6299b853f86c6afbd8b48c5aaa0a755c6594a5
Stefano Giaccio <sgiaccio@Stefanos-iMac.local>
2012-09-10 15:43:48 +0200

    Few small changes in js

--- src/main/webapp/js/unredd.js
+++ src/main/webapp/js/unredd.js
@@ -793,7 +793,7 @@ $(window).load(function () {
         }
     }
 
-    var closeLegend = function (scrollToId) {
+    var closeLegend = function () {
         $("#legend_pane").dialog('close');
         legendOn = false;
     }
@@ -870,7 +870,6 @@ $(window).load(function () {
     };
     
     setLayersTime = function (selectedDate) {
-        //console.log(layersJsonData); // DEBUG
         // loop through layers to see if they are time dependent
         $.each(UNREDD.timeDependentLayers, function (layerName, layer) {
             var sDates,
@@ -885,7 +884,7 @@ $(window).load(function () {
             for (i = 0; i < sDates.length; i++) {
                 d = new Date();
                 if (d.setISO8601(sDates[i])) {
-                    dates.push(d);                	
+                    dates.push(d);
                 }
             }
 
